cb1ac558a6104540b64ab3a062f2b5e48e7d4445
Michael Adelson <mike.adelson314@gmail.com>
2017-07-30 11:38:17 -0400

    Move StartSymbolAdder to preprocessing namespace

--- Forelle.Core.Tests/Parsing/Preprocessing/StartSymbolAdderTest.cs
+++ Forelle.Core.Tests/Parsing/Preprocessing/StartSymbolAdderTest.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Forelle.Parsing;
+using Forelle.Parsing.Preprocessing;
 
 namespace Forelle.Tests.Parsing.Preprocessing
 {
--- Forelle.Core/Parsing/Preprocessing/StartSymbolAdder.cs
+++ Forelle.Core/Parsing/Preprocessing/StartSymbolAdder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 
-namespace Forelle.Parsing
+namespace Forelle.Parsing.Preprocessing
 {
     internal static class StartSymbolAdder
     {
