6792efbeb8e790611df9913164865e6903f2f712
Michael Adelson <mike.adelson314@gmail.com>
2018-04-05 07:46:03 -0400

    Add invariant checks class

---
+++ Forelle.Core/Invariant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forelle
+{
+    internal static class Invariant
+    {
+        public static void Require(bool condition, string message = null)
+        {
+            if (!condition)
+            {
+                throw new InvariantViolatedException(message);
+            }
+        }
+    }
+
+    internal sealed class InvariantViolatedException : Exception
+    {
+        public InvariantViolatedException(string message)
+        {
+        }
+    }
+}
